4fb7c741b9f335c8fd7518f7b95605c4f49252ae
wesleysm <wesleys.miranda@live.com>
2016-11-02 16:49:27 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ echo "DIGITE A SENHA PARA O USUÁRIO ROOT:"
 passwd root
 echo ""
 mv /etc/sudoers /etc/sudoers.original
-cp sudoers /etc/sudoers
+cp sudoers /etc/
 chmod 440 /etc/sudoers
 echo ""
 echo "DEFINA O HOSTNAME PARA A MÁQUINA:"
 read hostname
 echo $hostname > /etc/hostname
 echo ""
-echo -e "pt_BR.UTF-8 UTF-8\npt_BR ISO-8859-1" > /etc/locale.gen 
+echo -e "pt_BR.UTF-8 UTF-8\npt_BR ISO-8859-1" >> /etc/locale.gen 
 locale-gen && export LANG=pt_BR.UTF-8
 locale > /etc/locale.conf
 ln -s /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
@@ -27,10 +27,10 @@ chmod 644 /etc/pacman.d/mirrorlist
 mv /etc/pacman.conf /etc/pacman.conf.original
 cp pacman.conf /etc/
 chmod 644 /etc/pacman.conf
-pacman -Sy linux-lts --noconfirm
-mkinitcpio -p linux-lts
+pacman -Sy linux-lts linux-lts-headers --noconfirm
 pacman -Sy grub efibootmgr --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable dhcpcd.service
+echo ""
 echo "INSTALAÇÃO CONCLUÍDA COM SUCESSO!"
