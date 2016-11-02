497677031107b685f4b66dc14ab72b00a950c177
wesleysm <wesleys.miranda@live.com>
2016-11-02 18:17:30 -0200

    Update usuario.sh

--- usuario.sh
+++ usuario.sh
@@ -3,3 +3,5 @@ echo "Informe o nome de login do usuário a ser criado:"
 read usuario
 useradd -m -s /bin/bash -g users -G audio,video,network,storage,wheel $usuario
 passwd $usuario
+pacman -Sy --noconfirm xdg-user-dirs
+xdg-user-dirs-update
