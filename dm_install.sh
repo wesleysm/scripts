ec5205ef4acea56d4fc7f1c9c07e5d20efea289d
wesleysm <wesleys.miranda@live.com>
2016-11-02 17:17:26 -0200

    Update dm_install.sh

--- dm_install.sh
+++ dm_install.sh
@@ -5,14 +5,16 @@ echo ""
 echo "[ 1 ] - GDM"
 echo "[ 2 ] - LIGHTDM"
 echo "[ 3 ] - SLIM"
-echo "[ 4 ] - SAIR"
+echo "[ 4 ] - LXDM"
+echo "[ 5 ] - SAIR"
 echo ""
 read op
 case $op in
 1) pacman -Sy --noconfirm gdm && systemctl enable gdm.service ;;
 2) pacman -Sy --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings && systemctl enable lightdm.service ;;
 3) pacman -Sy --noconfirm slim && systemctl enable slim.service ;;
-4) exit ;;
+4) pacman -Sy --noconfirm lxdm && systemctl enable lxdm.service ;; 
+5) exit ;;
 *) echo "Opção Inválida" ;;
 esac
 ./utilitarios
