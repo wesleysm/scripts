3f84f55b35029279c9cd4f519188c23a6cb368d3
wesleysm <wesleys.miranda@live.com>
2016-11-02 18:57:07 -0200

    Update utilitarios.sh

--- utilitarios.sh
+++ utilitarios.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pacman -Sy --noconfirm firefox firefox-i18n-pt-br mplayer smplayer numix-themes faenza-icon-theme clementine samba alsa-utils vim youtube-dl unrar p7zip zip gstreamer gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly ffmpeg
+pacman -Sy --noconfirm firefox firefox-i18n-pt-br mplayer smplayer numix-themes faenza-icon-theme clementine samba alsa-utils vim youtube-dl unrar p7zip zip gstreamer gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly ffmpeg eog evince file-roller
 #yaourt pamanc
 #yaourt numix-circle
 #clear
