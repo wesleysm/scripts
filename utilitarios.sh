1de3d57a45950bb959f4dd54c0e8942c1cb3af3c
wesleysm <wesleys.miranda@live.com>
2016-11-19 19:53:41 -0200

    Update utilitarios.sh

--- utilitarios.sh
+++ utilitarios.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pacman -Sy --noconfirm firefox firefox-i18n-pt-br mplayer smplayer numix-themes faenza-icon-theme clementine samba alsa-utils vim youtube-dl unrar p7zip zip gstreamer gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly ffmpeg eog evince file-roller ntfs-3g
+pacman -Sy --noconfirm firefox firefox-i18n-pt-br mplayer smplayer numix-themes faenza-icon-theme clementine samba alsa-utils alsa-lib vim youtube-dl unrar p7zip zip gstreamer gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly ffmpeg ntfs-3g networkmanager network-manager-applet && systemctl disable dhcpcd.service && systemctl enable NetworkManager.service
 ./usuario.sh
