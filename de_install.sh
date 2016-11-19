d0cc4e386c9181eadd8a7077dee803fefa244ca2
wesleysm <wesleys.miranda@live.com>
2016-11-19 19:46:59 -0200

    Update de_install.sh

--- de_install.sh
+++ de_install.sh
@@ -16,7 +16,7 @@ case $opcao in
 2) pacman -Sy --noconfirm xfce4 thunar-archive-plugin thunar-media-tags-plugin xfce4-datetime-plugin xfce4-notifyd xfce4-whiskermenu-plugin gnome-system-monitor gnome-calculator gedit eog evince gvfs gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb ;;
 3) pacman -Sy --noconfirm cinnamon nemo-share nemo-fileroller gvfs gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-smb gvfs-nfs gedit gnome-system-monitor gnome-calculator xfce4-terminal ;;
 4) pacman -Sy --noconfirm mate caja-open-terminal caja-sendto caja-share caja-wallpaper engrampa galculator-gtk2 mate-system-monitor mate-terminal mate-user-share mate-utils pluma mate-icon-theme-faenza ;;
-5) pacman -Sy plasma-desktop kde-l10n-pt_br breeze-gtk kde-gtk-config plasma-pa dolphin kdenetwork-filesharing systemd-kcm konsole plasma-nm ark kate kcalc plasma-workspace-wallpapers user-manager sddm-kcm ;;
+5) pacman -Sy --noconfirm plasma-desktop kde-l10n-pt_br breeze-gtk kde-gtk-config plasma-pa dolphin kdenetwork-filesharing systemd-kcm konsole plasma-nm ark kate kcalc plasma-workspace-wallpapers user-manager sddm-kcm ;;
 6) exit ;;
 *) echo "Opção Inválida" && ./de_install ;;
 esac
