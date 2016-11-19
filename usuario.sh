4db93580d66841981ea3e1096bd0b2cca8c1b202
wesleysm <wesleys.miranda@live.com>
2016-11-19 19:31:03 -0200

    Update usuario.sh

--- usuario.sh
+++ usuario.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+echo ""
 echo "Informe o nome de login do usuário a ser criado:"
 read usuario
-useradd -m -s /bin/bash -g users -G audio,video,network,storage,wheel $usuario
+useradd -m -s /bin/bash -g users -G adm,disk,audio,video,network,storage,wheel $usuario
+echo "Digite a senha para o usuário $usuario"
 passwd $usuario
 pacman -Sy --noconfirm xdg-user-dirs
 xdg-user-dirs-update
+mv /home/$usuario/.bashrc /home/$usuario/.bashrc.original
+cp bashrc /home/$usuario/.bashrc
