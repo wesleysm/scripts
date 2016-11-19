7a5a50f3e0f2cfb65bd8f01432b8faeae6d16c7b
wesleysm <wesleys.miranda@live.com>
2016-11-19 19:45:35 -0200

    Update de_install.sh

--- de_install.sh
+++ de_install.sh
@@ -12,12 +12,12 @@ echo ""
 read opcao
 echo ""
 case $opcao in
-1) pacman -Sy --noconfirm gnome-desktop gnome-shell gnome-terminal gedit gnome-system-monitor gnome-calculator gnome-shell-extensions nautilus-open-terminal nautilus gnome-control-center gnome-tweak-tool xdg-user-dirs-gtk eog evince gnome-calculator gnome-backgrounds gvfs gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb networkmanager network-manager-applet && systemctl disable dhcpcd.service && systemctl enable NetworkManager.service ;;
-2) pacman -Sy --noconfirm xfce4 thunar-archive-plugin thunar-media-tags-plugin xfce4-datetime-plugin xfce4-notifyd xfce4-whiskermenu-plugin gnome-system-monitor gnome-calculator gedit eog evince gvfs gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb xdg-user-dirs networkmanager network-manager-applet && systemctl disable dhcpcd.service && systemctl enable NetworkManager.service ;;
-3) pacman -Sy --noconfirm cinnamon nemo-share nemo-fileroller gvfs gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-smb gvfs-nfs networkmanager network-manager-applet gedit gnome-system-monitor gnome-calculator xfce4-terminal && systemctl disable dhcpcd.service && systemctl enable NetworkManager.service ;;
-4) pacman -Sy --noconfirm mate caja-open-terminal caja-sendto caja-share caja-wallpaper engrampa galculator-gtk2 mate-system-monitor mate-terminal mate-user-share mate-utils pluma mate-icon-theme-faenza networkmanager network-manager-applet && systemctl disable dhcpcd.service && systemctl enable NetworkManager.service ;;
-5) pacman -Sy plasma-desktop kde-l10n-pt_br breeze-gtk kde-gtk-config plasma-pa dolphin kdenetwork-filesharing systemd-kcm konsole plasma-nm ark kate kcalc plasma-workspace-wallpapers networkmanager && systemctl enable NetworkManager.service;;
+1) pacman -Sy --noconfirm gnome-desktop gnome-shell gnome-terminal gedit gnome-system-monitor gnome-calculator gnome-shell-extensions nautilus-open-terminal nautilus gnome-control-center gnome-tweak-tool eog evince gnome-calculator gnome-backgrounds gvfs gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb ;;
+2) pacman -Sy --noconfirm xfce4 thunar-archive-plugin thunar-media-tags-plugin xfce4-datetime-plugin xfce4-notifyd xfce4-whiskermenu-plugin gnome-system-monitor gnome-calculator gedit eog evince gvfs gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb ;;
+3) pacman -Sy --noconfirm cinnamon nemo-share nemo-fileroller gvfs gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-smb gvfs-nfs gedit gnome-system-monitor gnome-calculator xfce4-terminal ;;
+4) pacman -Sy --noconfirm mate caja-open-terminal caja-sendto caja-share caja-wallpaper engrampa galculator-gtk2 mate-system-monitor mate-terminal mate-user-share mate-utils pluma mate-icon-theme-faenza ;;
+5) pacman -Sy plasma-desktop kde-l10n-pt_br breeze-gtk kde-gtk-config plasma-pa dolphin kdenetwork-filesharing systemd-kcm konsole plasma-nm ark kate kcalc plasma-workspace-wallpapers user-manager sddm-kcm ;;
 6) exit ;;
-*) echo "Opção Inválida" ;;
+*) echo "Opção Inválida" && ./de_install ;;
 esac
 ./dm_install.sh
