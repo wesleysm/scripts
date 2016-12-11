3b2d04e4728acc27ae4ad556ae4d4a5fd680f5bb
wesleysm <wesleys.miranda@live.com>
2016-12-11 09:21:43 -0200

    Update vmware-guest.sh

--- vmware-guest.sh
+++ vmware-guest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pacman -Sy open-vm-tools xf86-video-vmware
+pacman -Sy open-vm-tools xf86-video-vmware gtkmm
 cat /proc/version > /etc/arch-release
 echo needs_root_rights=yes > /etc/X11/Xwrapper.config
 systemctl enable vmtoolsd.service
